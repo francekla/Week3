0d0e87083b0f41f3f68bda2e8d45257bc6cb95a9
francekla <akandondafx@gmail.com>
2025-05-24 21:23:13 +0300

    Finalizing Changes

---
+++ Assignment2.py
@@ -0,0 +1,41 @@
+import pandas as pd 
+import numpy as np 
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+# This is my first assignment for the course
+# I will create an environment ds_env
+# Install the necessary libraries
+# Export the setup to a requirements.txt file
+
+# Open new project in vscode
+# Create a new file called Assignment2.py
+# Open Terminal using git bash (Recommended)
+# create a directory Week3
+# cd into Week3
+
+# Configure my git user name and email
+# create a new repository called Assignment2
+# add the remote repository
+# Create a virtual environment called ds_env
+# Activate the virtual environment
+# Install the necessary libraries usin pip install pandas numpy matplotlib seaborn
+
+# Check the installed libraries using pip list
+# Git the changes using git add .
+# Commit the changes using git commit -m "Initial commit"
+# Push the changes to the remote repository using git push origin main
+# Create a requirements.txt file using pip freeze > requirements.txt
+# Deactivate the virtual environment using deactivate
+# Push the requirements.txt file to the remote repository using git push origin main
+
+#DOne with the assignment
+
+
+
+
+
+
+
+
